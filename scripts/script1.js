7fa0ec50a105676fe0c4fdace85e5579c18e13d6
Nirjhar Deb <62759223+Noltan@users.noreply.github.com>
2021-12-25 11:27:21 -0500

    removed firebase

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,6 +1,3 @@
-//Reference messages collection
-var messagesRef = firebase.database().ref('messages');
-
 var helloText = ["Hello!", "¡Hola!", "Hallo!", "Olá!", "Ciao!", "Namaste!", "!سلام", "Halló!", "Hej!"];
 var helloCounter = 0;
 var helloRepeatMethod = setInterval(changeHello,2000);
@@ -19,37 +16,10 @@ window.onload = function () {
     changeHello();
 };
 
-//Listen to submit event from contact form
-var submitCheck = document.getElementById("submitButton");
-if(submitCheck) {
-    submitCheck.addEventListener("submit", submitForm);
-}
+//Listen for a submit in the contact form
+document.querySelector("contact-form").addEventListener("submit", submitForm);
 
-//Submit form
 function submitForm(e) {
     e.preventDefault();
-    
-    //get values
-    var name = getInputVal("name");
-    var email = getInputVal("email");
-    var phoneNumber = getInputVal("phoneNumber");
-    var message = getInputVal("message");
-
-    saveMessage(name,email,phoneNumber,message);
-}
-
-//Get contact form values
-function getInputVal(id) {
-    return document.getElementById(id).value;
-}
-
-//Save messages to firebase
-function saveMessage(name,email,phoneNumber,message) {
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-        name: name,
-        email: email,
-        phoneNumber: phoneNumber,
-        message: message
-    });
+    console.log(123);
 }
