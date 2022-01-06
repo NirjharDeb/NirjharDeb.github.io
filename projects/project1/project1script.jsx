5a8648b057d7f12d02537a4d920ccdc4145baf55
Nirjhar Deb <62759223+Noltan@users.noreply.github.com>
2022-01-06 00:46:06 -0500

    Simple greeting designed in react.js

--- projects/project1/project1script.jsx
+++ projects/project1/project1script.jsx
@@ -3,16 +3,26 @@ class ChatBot extends React.Component {
   constructor(props) {
     super(props);
     this.state = {value: ""};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
+  handleSubmit(event) {
+    alert('Hello ' + this.state.value + '!');
+    event.preventDefault();
+  }
   
   render() {
     return (
       <div>
         <h1>A React Chatbot (Work in Progress)</h1>
         <p>Built by Nirjhar</p>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Name:
             <input type="text" value={this.state.value} onChange={this.handleChange} />
